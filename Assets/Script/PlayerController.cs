67701a50cd4ed6f3037a6238f481e243ba985981
Nam Hoang <hainam8x@gmail.com>
2020-06-12 08:29:47 +0700

    update

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Rigidbody2D theRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw, 1f);
+        
+    }
+}
